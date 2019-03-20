41715a698f9c6485773a5aebec0bd3427eca6cd2
liangzijian <645383348@qq.com>
2019-03-21 03:18:34 +0800

    封装icon组件并为button添加icon组件

--- src/Button.vue
+++ src/Button.vue
@@ -1,10 +1,25 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <g-icon v-if="icon" class="icon" :name="icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
-    name: "Button"
+    name: "Button",
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return !(value !== 'left' && value !== 'right')
+        }
+      }
+    }
   }
 </script>
 
@@ -16,6 +31,10 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: top;
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -24,6 +43,24 @@
     }
     &:focus {
       outline: none;
+    }
+    > .content {
+      order: 2;
+      margin-bottom: .1em;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .2em;
+    }
+    &.icon-right {
+      > .content {
+        order: 1
+      }
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .2em;
+      }
     }
   }
 </style>
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Buttom from './Button'
+import icon from './icon'
 
 Vue.component('g-button', Buttom)
+Vue.component('g-icon', icon)
 
 new Vue({
   el: '#app'
