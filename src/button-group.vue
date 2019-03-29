9ba5e3e0a97fa5e35b5a322e9a493a52bf5d71fb
liangzijian <645383348@qq.com>
2019-03-30 02:37:04 +0800

    添加scoped, 格式化scss

--- src/button-group.vue
+++ src/button-group.vue
@@ -19,11 +19,26 @@
 </script>
 
 <style lang="scss" scoped>
-  .s-button-group {display: inline-flex;vertical-align: top;
-    > .g-button {border-radius: 0; &:not(:first-child) {margin-left: -1px;}
-      &:first-child {border-top-left-radius: var(--border-radius);border-bottom-left-radius: var(--border-radius);}
-      &:last-child {border-top-right-radius: var(--border-radius);border-bottom-right-radius: var(--border-radius);}
-      &:hover {position: relative;z-index: 1;}
+  .s-button-group {
+    display: inline-flex;
+    vertical-align: top;
+    > .s-button {
+      border-radius: 0;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
     }
   }
 </style>
