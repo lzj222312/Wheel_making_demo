9baa31287cbbc2609ce41cdd299259f23e1bb993
liangzijian <645383348@qq.com>
2019-03-31 20:13:54 +0800

    添加input组件测试用例

---
+++ test/input.test.js
@@ -0,0 +1,102 @@
+const { expect } = chai
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+  it('Input是否存在', () => {
+    expect(Input).to.exist
+  })
+
+  describe('props', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+
+    afterEach(() => {
+      vm.$destroy()
+    })
+
+    it('value', () => {
+      vm = new Constructor({
+        propsData: {
+          value: '1234'
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.value).to.equal('1234')
+    })
+
+    it('disabled', () => {
+      vm = new Constructor({
+        propsData: {
+          disabled: false
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.disabled).to.equal(false)
+    })
+
+    it('readonly', () => {
+      vm = new Constructor({
+        propsData: {
+          readOnly: false
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.readOnly).to.equal(false)
+    })
+
+    it('error', () => {
+      vm = new Constructor({
+        propsData: {
+          error: "姓名不能少于2个字"
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+      const errorMessage = vm.$el.querySelector('.error-message')
+      expect(errorMessage.innerHTML).to.equal('姓名不能少于2个字')
+    })
+  })
+
+  describe('event', () => {
+    const Constructor = Vue.extend(Input)
+    let vm
+
+    afterEach(() => {
+      vm.$destroy()
+    })
+
+    it('input/change', () => {
+      ['input', 'change'].forEach(eventName => {
+        vm = new Constructor({}).$mount()
+        const callback = sinon.fake()
+        vm.$on(eventName, callback)
+        // 手动触发事件
+        let event = new Event(eventName)
+        Object.defineProperty(event, 'target', {
+          value: { value: 'hi' },
+          enumerable: true
+        })
+        let inputElement = vm.$el.querySelector('input')
+        inputElement.dispatchEvent(event)
+        expect(callback).to.have.been.calledWith('hi')
+      })
+    })
+
+    it('blur/focus', () => {
+      ['blur', 'focus'].forEach(eventName => {
+        vm = new Constructor({}).$mount()
+        const callback = sinon.fake()
+        vm.$on(eventName, callback)
+        // 手动触发事件
+        let event = new Event(eventName)
+        let inputElement = vm.$el.querySelector('input')
+        inputElement.dispatchEvent(event)
+        expect(callback).to.have.been.calledWith(event)
+      })
+    })
+  })
+})
